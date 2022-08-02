5be614ae807330bfb5a9e8758443cc8ffae60ac9
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2022-08-01 22:52:29 -0400

    Initial work

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,5 +1,9 @@
 package io.papermc.testplugin;
 
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandBuilder;
+import io.papermc.paper.command.brigadier.argument.BlockArgument;
+import io.papermc.paper.command.brigadier.argument.EnchantmentArgument;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,5 +12,16 @@ public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        CommandBuilder.of(this, "hello")
+            .then(
+                RequiredArgumentBuilder.argument("getreal", new EnchantmentArgument())
+            ).then(
+                RequiredArgumentBuilder.argument("getrealy", new BlockArgument())
+            )
+            .aliases("wow", "bob", "weird spaces", "oog")
+            .register();
     }
+
+
 }
