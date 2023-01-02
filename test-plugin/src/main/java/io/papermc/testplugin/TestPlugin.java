ac9646a6ec71bfa212ed90bfcd3b9bbcc43c4092
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2023-01-02 13:13:21 -0500

    Support dispatcher reloading with bukkit commands

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -4,24 +4,32 @@ import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.argument.VanillaArguments;
+import io.papermc.paper.event.server.ServerResourcesReloadedEvent;
 import io.papermc.paper.testplugin.example.ExampleAdminCommand;
 import io.papermc.paper.testplugin.example.IceCreamType;
 import io.papermc.paper.testplugin.example.IceCreamTypeArgument;
+import org.bukkit.Bukkit;
 import org.bukkit.block.BlockState;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+// TODO: Move this to plugin bootstrapping
 public final class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void reload(ServerResourcesReloadedEvent event) {
         ExampleAdminCommand.register(this);
 
         this.getServer().getCommandMap().register("fallback", new BukkitCommand("hi", "cool hi command", "<>", List.of("hialias")) {
@@ -34,6 +42,10 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getCommandMap().getKnownCommands().values().removeIf((command) -> {
             return command.getName().equals("hi");
         });
+        // Occurs after commands can be reloaded, for now
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.updateCommands();
+        }
     }
 
 
