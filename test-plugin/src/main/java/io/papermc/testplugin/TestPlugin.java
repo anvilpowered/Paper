6990b146d6681f371c45b0ead8d8748ae465f503
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-11-24 14:30:52 -0800

    demo removing Position from Location

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,13 +1,7 @@
 package io.papermc.testplugin;
 
 import io.papermc.paper.event.player.ChatEvent;
-import io.papermc.paper.math.Position;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Block;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,15 +15,16 @@ public final class TestPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onChat(ChatEvent event) {
-        final Location loc = event.getPlayer().getLocation();
-        loc.subtract(0, 2, 0);
-        BiFunction<World, Location, Block> function = World::getBlockAt;
-        System.out.println(function.apply(event.getPlayer().getWorld(), loc));
-
-        final Chunk chunk = event.getPlayer().getWorld().getChunkAt(loc);
-        System.out.println(chunk);
-
-        final Location rod = event.getPlayer().getWorld().findLightningRod(loc);
-        System.out.println(rod);
+        System.out.println(Bukkit.getServer().isOwnedByCurrentRegion(event.getPlayer().getWorld(), event.getPlayer().getLocation()));
+        // final Location loc = event.getPlayer().getLocation();
+        // loc.subtract(0, 2, 0);
+        // BiFunction<World, Location, Block> function = World::getBlockAt;
+        // System.out.println(function.apply(event.getPlayer().getWorld(), loc));
+        //
+        // final Chunk chunk = event.getPlayer().getWorld().getChunkAt(loc);
+        // System.out.println(chunk);
+        //
+        // final Location rod = event.getPlayer().getWorld().findLightningRod(loc);
+        // System.out.println(rod);
     }
 }
